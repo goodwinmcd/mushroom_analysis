fc47cc596a6db6f2a4b3f199898859c98361ae0b
goodwin <goodwinmcd@gmail.com>
2018-12-05 20:45:19 -0700

    treated the data and fit it to linear regression model

--- classification.py
+++ classification.py
@@ -1,7 +1,28 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
 
-mushrooms = pd.read_csv('C:\\Users\\mcdonago\\source\\repos\\mushroom_data\\mushroom_data\\mushrooms.csv')
+mushrooms = pd.read_csv('/home/goodwin/Documents/Projects/mushroom_analysis/mushrooms.csv')
 
+def treat_data(data_feature):
+    number = LabelEncoder()
+    data_feature = number.fit_transform(data_feature.astype('str'))
+    return data_feature
+
+def treat_all_data(data):
+    for column in data.columns:
+        data[column] = treat_data(data[column])
+    return data
+
+mushrooms = treat_all_data(mushrooms)
 x_data = mushrooms.drop(columns=['class'])
 y_data = mushrooms['class']
 
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=.2)
+
+lg = LogisticRegression()
+lg.fit(x_train, y_train)
+accuracy = lg.score(x_test, y_test)
+print('Accuracy of logistic regression: {}'.format(accuracy))
