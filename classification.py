7a91fc133147f67de8eef46a9193857a4e7ebfb3
Goodwin McDonald <gmcdonald@vertafore.com>
2018-12-07 16:02:18 -0700

    Added functions to fit the models and print their accuracy

--- classification.py
+++ classification.py
@@ -1,11 +1,14 @@
 import pandas as pd
+import numpy as np
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder
-import numpy as np
 from sklearn.naive_bayes import GaussianNB
-
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 
 mushrooms = pd.read_csv(os.environ['mushroom_data'])
 
@@ -19,20 +22,29 @@ def treat_all_data(data):
         data[column] = treat_data(data[column])
     return data
 
+def fit_data(model_array, x_data, y_data):
+    for model in model_array:
+        model_array[model].fit(x_data, y_data)
+
+def print_accuracy(model_array, x_test_data, y_test_data):
+    for model in model_array:
+        accuracy = model_array[model].score(x_test_data, y_test_data)
+        print('Accuracy of {}: {}'.format(model, accuracy))
+
 mushrooms = treat_all_data(mushrooms)
 x_data = mushrooms.drop(columns=['class'])
 y_data = mushrooms['class']
 
-print(mushrooms.corr())
-
 x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=.2)
 
-lg = LogisticRegression()
-gnb = GaussianNB()
+models = {'Logistic Regression': LogisticRegression(), 
+          'Gaussian naive bayes': GaussianNB(), 
+          'Support vector machine': SVC(), 
+          'Decision tree classifier': DecisionTreeClassifier(), 
+          'Random Forest Classifier': RandomForestClassifier(), 
+          'Neural Network MLP': MLPClassifier()
+          }
+fit_data(models, x_train, y_train)
+print_accuracy(models, x_test, y_test)
 
-lg.fit(x_train, y_train)
-gnb.fit(x_train, y_train)
-lg_accuracy = lg.score(x_test, y_test)
-gnb_accuracy = gnb.score(x_test, y_test)
-print('Accuracy of logistic regression: {}'.format(lg_accuracy))
-print('Accuracy of gausian naive bayes regression: {}'.format(gnb_accuracy))
+
